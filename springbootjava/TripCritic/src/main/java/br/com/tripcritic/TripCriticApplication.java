4f3c936fbc1d7f94d8af339932dd5c719c5071ef
Vinícius Felipe <vfb2@cin.ufpe.br>
2025-09-21 00:40:02 -0300

    feat: inclui projeto Spring Boot para fins de estudo e referência
    Este projeto tem como objetivo oferecer uma visão mais ampla sobre o Spring Boot, complementando os pontos que não puderam ser explorados durante a apresentação em sala de aula.

---
+++ springbootjava/TripCritic/src/main/java/br/com/tripcritic/TripCriticApplication.java
@@ -0,0 +1,13 @@
+package br.com.tripcritic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripCriticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripCriticApplication.class, args);
+	}
+
+}
---
+++ springbootjava/TripCritic/src/test/java/br/com/tripcritic/TripCriticApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tripcritic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripCriticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
